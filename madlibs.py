053dbe0fe194c5a57cb09a8250a97960b8f96417
rotary26B <David.Stern2@marist.edu>
2018-09-06 15:13:04 -0400

    Solution to Mad Libs exercise.

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#CMPT 120L 113
+#Mad Libs exercise
+#Author: D. Stern
+#6 September 2018
+
+def MadLibs():
+    x = input("Enter a noun: ")
+    y = input("Enter a verb: ")
+    z = input("Enter an adjective: ")
+    a = input("Enter a place: ")
+
+    print("Take your " + z + " " + x + " and " + y + " it at the " + a + "!")
+MadLibs()
