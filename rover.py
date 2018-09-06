b25df820762bf4e87d484275c834f7873729c452
rotary26B <David.Stern2@marist.edu>
2018-09-06 14:30:08 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,14 @@
+#Rover Program
+# 6 September 2018
+# Author: David Stern
+
+def TimeCalculation():
+    light = 186000
+    distance = 34000000
+    time = distance/light
+
+    print("It will take " + str(time) + " seconds to receive an image from Curiosity.")
+
+
+TimeCalculation()
+ 
